1a6ba373edc65e5a3ac8af71a3aa4cb2a0edb1ae
Umang375 <ujain7315@gmail.com>
2023-03-02 01:14:10 +0530

    navabr changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
@@ -7,13 +6,14 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import { Link } from 'react-router-dom';
+import './Navbar.scss'
 
 
 const Navbar = () => {
   return (
     <div className='navBar'>
         <div className="left">
-          <Link to = "/" style = {{textDecoration:"none"}}>
+          <Link to = "/" style={{textDecoration : "none"}}>
           <span>
           Real Social
           </span>
@@ -22,14 +22,18 @@ const Navbar = () => {
           <DarkModeOutlinedIcon/>
           <GridViewOutlinedIcon/>
           <div className="search">
-            <SearchOutlinedIcon/>
             <input type="text" placeholder='search' />
+            <SearchOutlinedIcon/>
           </div>
         </div>
         <div className="right">
           <PersonOutlineOutlinedIcon/>
           <NotificationsOutlinedIcon/>
           <EmailOutlinedIcon/>
+          <div className='user'>
+            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <span>John Doe</span>
+          </div>
         </div>
     </div>
   )
