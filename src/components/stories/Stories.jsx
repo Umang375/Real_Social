d1654ed465ff6fc100c36690dffa09b48eb7b92f
Umang375 <ujain7315@gmail.com>
2023-03-14 00:25:27 +0530

    adding test data

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 import './stories.scss';
 
 const Stories = () => {
+
+    //Testing Data
+
+    const stories = [
+        {
+          id: 1,
+          name: "John Doe",
+          img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+        },
+        {
+          id: 2,
+          name: "John Doe",
+          img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+        },
+        {
+          id: 3,
+          name: "John Doe",
+          img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+        },
+        {
+          id: 4,
+          name: "John Doe",
+          img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+        },
+      ];
   return (
     <div className='stories'>
       
