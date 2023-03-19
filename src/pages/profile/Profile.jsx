b7abca535666087ab8a9055c594429d06d3c4069
Umang375 <ujain7315@gmail.com>
2023-03-20 00:15:56 +0530

    desinged profile page UI

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React from "react";
+import { FacebookTwoTone } from "@mui/icons-material";
+import {Twitter} from "@mui/icons-material";
+import { Instagram } from "@mui/icons-material";
+import { Pinterest } from "@mui/icons-material";
+import { Place } from "@mui/icons-material";
+import { Language } from "@mui/icons-material";
+import EmailOutlined from "@mui/icons-material/EmailOutlined";
+import { MoreVert } from "@mui/icons-material";
+import { LinkedIn } from "@mui/icons-material";
+import "./profile.scss";
+import Posts from "../../components/posts/Posts";
 
 const Profile = () => {
   return (
-    <div>
-      profile one
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          className="dp"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoTone fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <Instagram fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <Twitter fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedIn fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <Pinterest fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Jane Doe</span>
+            <div className="info">
+              <div className="item">
+                <Place/>
+                <span>India</span>
+              </div>
+              <div className="item">
+                <Language/>
+                <span>umang.dev</span>
+              </div>
+            </div>
+              <button>Follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlined />
+            <MoreVert />
+          </div>
+        </div>
+      <Posts/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
