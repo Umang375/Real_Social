77e61faa33891aa739e8d16ea5ed427bb1a4ca3a
Umang375 <ujain7315@gmail.com>
2023-03-29 23:20:27 +0530

    added routes for users

--- api/index.js
+++ api/index.js
@@ -1,14 +1,21 @@
+// import userRoute from './routes/users.js';
+const userRoute = require('./routes/users');
 const express = require('express');
-const router = express.Router();
+// const router = express.Router();
 const app = express();
 const mysql = require('mysql');
+const PORT = 5000;
 
 mysql.createConnection({
     host: 'localhost',
-    
+    name: 'root',
+    password: 'Ujain7315@',
+    database: 'test'
 })
 app.use(express.json());
 
-app.listen(5000,()=>{
+app.use('/api/users', userRoute);
+
+app.listen(PORT,()=>{
     console.log("Server Online");
 })
--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const mysql = require('mysql');
 const app = express();
 
+router.get('/test',(req,res) =>{
+    res.send("Hello World");
+})
 
+module.exports = router;
 
-export default router;
+// export default router;
+// use this when you are using ES6/ type is module
